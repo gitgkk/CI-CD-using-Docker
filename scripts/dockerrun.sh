6c5c443fcf989409e1fda6866c8cff68536e136c
gitgkk <gajendrakkashyap@gmail.com>
2023-03-16 18:09:45 +0530

    Update dockerrun.sh

--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,3 +1,3 @@
-sudo docker pull gajendrakashyap/samplejavaapp:latest
+sudo docker pull gajendrakashyap/gk-docks:latest
 sudo docker rm -f javaapplication
-sudo docker run --name javaapplication -itd -p 8003:8080 gajendrakashyap/samplejavaapp:latest
+sudo docker run --name javaapplication -itd -p 8003:8080 gajendrakashyap/gk-docks:latest
