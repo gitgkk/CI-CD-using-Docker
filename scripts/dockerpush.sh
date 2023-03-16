75551ddfd4f2566c5f5d4c7d84b0e29485218e3a
gitgkk <gajendrakkashyap@gmail.com>
2023-03-16 18:08:49 +0530

    Update dockerpush.sh

--- scripts/dockerpush.sh
+++ scripts/dockerpush.sh
@@ -1,2 +1,2 @@
 sudo docker login --username=$username --password=$password
-sudo docker push docker push gajendrakashyap/gk-docks:java-app
+sudo docker push docker push gajendrakashyap/gk-docks:latest
